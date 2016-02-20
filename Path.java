a7090d7a631db3acda0b3ef8d212bd65e409170c
SerkanDemirci <serkan.demirci@ug.bilkent.edu.tr>
2016-02-20 17:06:51 +0200

    Update Path.java

--- Path.java
+++ Path.java
@@ -1,31 +1,42 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Path {
+//Path is a list consisting of the states inside the state-space.
+public class Path
+{
 	public List<State> states;
 
-	public Path() {
+	// Default Constructors
+	public Path()
+	{
 		states = new ArrayList<>();
 	}
 
-	public Path(State state) {
+	// Creates a path with initial starting state
+	public Path(State state)
+	{
 		states = new ArrayList<>();
 		states.add(state);
 	}
 
-	public Path(Path copy, State nextState) {
+	// Creates a new path, extending the given path with the next state.
+	public Path(Path copy, State nextState)
+	{
 		states = new ArrayList<>(copy.states);
 		states.add(nextState);
 	}
+
+	// Returns the last state of the path
 	public State getLast()
 	{
-		return states.get(states.size()-1);
+		return states.get(states.size() - 1);
 	}
-	
+
 	public String toString()
 	{
 		StringBuilder builder = new StringBuilder();
-		builder.append("Missionary\tCannibal\tBoat Position\n");
+		builder.append("\t    Left\t\t\t\t\t    Right\n");
+		builder.append("Missionaries\tCannibals\tBoat Position\tMissionaries\tCannibals\n");
 		for (State s : states)
 		{
 			builder.append(s);
